4a40077d7c5dffc07f35e27d0b017ed36952ab85
Arthur Maltson <arthur.kalm@gmail.com>
2013-11-05 21:58:20 -0500

    Upgrade Adium

--- sprout-osx-apps/attributes/adium.rb
+++ sprout-osx-apps/attributes/adium.rb
@@ -1,3 +1,3 @@
-default['sprout']['adium']['dmg']['source']      = 'http://sourceforge.net/projects/adium/files/Adium_1.5.7.dmg/download'
-default['sprout']['adium']['dmg']['checksum']    = 'f80408a2c5bc97be8adb35ffeb7c4501392e847edae4e6e24d87da72de7e96b9'
-default['sprout']['adium']['dmg']['volumes_dir'] = 'Adium 1.5.7'
+default['sprout']['adium']['dmg']['source']      = 'http://sourceforge.net/projects/adium/files/Adium_1.5.8.dmg/download'
+default['sprout']['adium']['dmg']['checksum']    = '4d66db781b5456eea6939b35cd47866ecacbd05686eead77dc406d2ddbde02eb'
+default['sprout']['adium']['dmg']['volumes_dir'] = 'Adium 1.5.8'
